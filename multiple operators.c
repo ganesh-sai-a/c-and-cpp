964cd779c8652a089bcd95aebdaaffc2691626d3
ganesh-sai-a <68336907+ganesh-sai-a@users.noreply.github.com>
2020-07-15 19:51:41 +0530

    Add files via upload

---
+++ multiple operators.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,sum;
+	printf("enter values of a, b, c, d");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	sum=a+b+c+d;
+	printf("sum of a, b, c, d is %d",sum);
+	return 0;
+}
