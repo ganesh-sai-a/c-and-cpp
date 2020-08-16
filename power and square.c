33a33f4ff21d54ac9c5bc8b2c68db879e5529c89
ganesh-sai-a <68336907+ganesh-sai-a@users.noreply.github.com>
2020-08-16 13:25:03 +0530

    Add files via upload

---
+++ power and square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float x,y;
+	printf("enter the values of a and y :");
+	scanf("%f%f",&x,&y);
+	printf("x raised to power y is : %f \n",pow(x,y));
+	printf("square root of x is : %f \n",sqrt(x));
+	printf("square root of y is : %f \n",sqrt(y));
+	return 0;
+}
