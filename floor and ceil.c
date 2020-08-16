1c714f529ac3ff6d4dd492dbc2d405b05eba4e67
ganesh-sai-a <68336907+ganesh-sai-a@users.noreply.github.com>
2020-08-16 13:18:31 +0530

    Add files via upload

---
+++ floor and ceil.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float x;
+	printf("enter value of x :");
+	scanf("%f",&x);
+	printf("floor value of x is : %f \n",floor(x));
+	printf("ceil value of x is : %f \n",ceil(x));
+	return 0;
+}
+
